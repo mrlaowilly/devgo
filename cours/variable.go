818873c68479d02bc32239028d79aa24042445cf
mrlaowilly <willy.meksavanh@hotmail.fr>
2022-05-02 10:11:26 +0200

    first commit

---
+++ cours/variable.go
@@ -0,0 +1,10 @@
+package cours
+
+var leNomDeLaVariablePrivate string // nil
+var LeNomDeLaVariablePublic string  // nil
+
+var leNomDeLaVariablePrivateAvecValue = "Hello"
+var LeNomDeLaVariablePublicAvecValue = "Hello"
+
+var leNomDeLaVariablePrivateAvecValueEtType string = "Hello"
+var leNomDeLaVariablePublicAvecValueEtType string = "Hello"
